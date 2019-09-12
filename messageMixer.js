eac2ff37851d578d4315e7df278c9c2599376753
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-11 21:03:46 -0400

    capitalize the first character of words

--- messageMixer.js
+++ messageMixer.js
@@ -10,3 +10,12 @@ const countCharacter = function (inputString, inputCharacter) {
     }
     return count;
 };
+// capitalize the first character of words
+const capitalizeFirstCharacterOfWords = function (string) {
+    let arr = string.split(" ");
+    for (let i = 0; i < arr.length; i++) {
+        let word = arr[i];
+        arr[i] = word[0].toUpperCase() + word.substring(1);
+    }
+    return arr.join(" ");
+};
