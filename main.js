1b0b9a8e9c9204f0d24fc721e9fec2e1d8939fbb
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-11 21:42:44 -0400

    displaying the imported functions

--- main.js
+++ main.js
@@ -9,3 +9,19 @@ import {
     palindrome,
     pigLatin
 } from './messageMixer';
+// displaying the functions
+function displayMessage() {
+    let str = 'What is the color of the sky?';
+    console.log('countCharacter: ' + countCharacter(str, 't'));
+    console.log('capitalized: ' + capitalizeFirstCharacterOfWords(str));
+    console.log('reverseWord: ' + reverseWord(str));
+    console.log('reverseAll: ' + reverseAllWords(str));
+    console.log('replaceFirst: ' + replaceFirstOccurence(str, 'sky', 'water'));
+    console.log('replaceAll: ' + replaceAllOccurences(str, 'sky', 'water'));
+    console.log('encode: ' + encode(str));
+    console.log('palindrome: ' + palindrome(str));
+    console.log('pigLatin: ' + pigLatin(str, '@'));
+};
+
+
+displayMessage();
